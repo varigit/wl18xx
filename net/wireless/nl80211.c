03ed8bfec9c529571e87dd641a94ded404e9505f
Eliad Peller <eliad@wizery.com>
2015-03-09 10:29:08 +0200

    Revert "nl80211: Allow set network namespace by fd" (TEMP)
    temporarily revert this patch until the missing EXPORT_SYMBOL
    will be added to backports.
    
    Signed-off-by: Eliad Peller <eliad@wizery.com>

--- net/wireless/nl80211.c
+++ net/wireless/nl80211.c
@@ -7774,19 +7774,14 @@ static int nl80211_wiphy_netns(struct sk_buff *skb, struct genl_info *info)
 	struct cfg80211_registered_device *rdev = info->user_ptr[0];
 	struct net *net;
 	int err;
+	u32 pid;
 
-	if (info->attrs[NL80211_ATTR_PID]) {
-		u32 pid = nla_get_u32(info->attrs[NL80211_ATTR_PID]);
-
-		net = get_net_ns_by_pid(pid);
-	} else if (info->attrs[NL80211_ATTR_NETNS_FD]) {
-		u32 fd = nla_get_u32(info->attrs[NL80211_ATTR_NETNS_FD]);
-
-		net = get_net_ns_by_fd(fd);
-	} else {
+	if (!info->attrs[NL80211_ATTR_PID])
 		return -EINVAL;
-	}
 
+	pid = nla_get_u32(info->attrs[NL80211_ATTR_PID]);
+
+	net = get_net_ns_by_pid(pid);
 	if (IS_ERR(net))
 		return PTR_ERR(net);
 
